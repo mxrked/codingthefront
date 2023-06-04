6a3db9847f4d71d52d17cf5cee52ea169fbdd288
Parker <79803353+mxrked@users.noreply.github.com>
2023-06-04 08:59:09 -0400

    Delete assets directory

--- assets/data/variables/REGEX.js
+++
@@ -1,5 +0,0 @@
-// This file is used to hold some common regex patterns
-
-const EMAIL_VALIDATION = "^[a-zA-Z0–9+_.-]+@[a-zA-Z0–9.-]+$";
-const EMPTY_STRING = "^\s*$";
-const STRONG_PASSWORD = "(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s)(?=.*[!@#$*])"
