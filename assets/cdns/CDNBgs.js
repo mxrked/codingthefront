ad808e93f5c85e5119270ac0086c3b6b8495f609
Parker <pp101socials@gmail.com>
2023-07-01 09:39:11 -0400

    Added cdns

--- assets/cdns/CDNBgs.js
+++ assets/cdns/CDNBgs.js
@@ -6,4 +6,7 @@
 
 import { CDNBGReturn } from "./CDNReturns";
 
-export {};
+const INDEX_INFO_BG = CDNBGReturn("index", "index-info-bg", "webp");
+const INDEX_CONTACT_BG = CDNBGReturn("index", "index-contact-bg", "webp");
+
+export { INDEX_CONTACT_BG, INDEX_INFO_BG };
