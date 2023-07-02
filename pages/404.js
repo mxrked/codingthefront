20f4b88d12c9b82ca601f4fdecb2d6f5844a8520
Parker <pp101socials@gmail.com>
2023-07-02 10:30:30 -0400

    Created file

---
+++ pages/404.js
@@ -0,0 +1,22 @@
+// React/Next Imports
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+// Library Imports
+
+// Data/Functions/Images Imports
+
+// Component Imports
+
+// Style Imports
+import "../assets/styles/modules/404/404.module.css";
+
+export default function NotFound() {
+  const router = useRouter();
+
+  return (
+    <div id="PAGE" className="page overrides_404 full-second">
+      <main id="PAGE_CNT"></main>
+    </div>
+  );
+}
