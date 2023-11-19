7252adbebf4f9c6d2d013d3e94d43bb07193ef70
Parker <pp101socials@gmail.com>
2023-11-19 09:30:09 -0500

    Created file

--- assets/functions/dom/closers/CloseMobileNav.js
+++ assets/functions/dom/closers/CloseMobileNav.js
@@ -6,4 +6,29 @@
 
 import RemoveStorageVariable from "../../data/storage/RemoveStorageVariable";
 
-export default function CloseMobileNav() {}
+export default function CloseMobileNav() {
+  const MENU = document.getElementById("mobileNavMenu");
+  const DARKEN = document.getElementById("mobileNavMenuDarken");
+  const MENU_MAIN = document.getElementById("mobileNavMenuMain");
+  const MENU_MAIN_CNT = document.getElementById("mobileNavMenuMainCnt");
+
+  MENU.style.pointerEvents = "none";
+  MENU.style.overflowY = "hidden";
+
+  setTimeout(() => {
+    MENU_MAIN_CNT.style.opacity = 0;
+    MENU_MAIN_CNT.style.visibility = "hidden";
+  }, 600);
+
+  setTimeout(() => {
+    MENU_MAIN.style.transform = "translateX(100%)";
+    DARKEN.style.opacity = 0;
+    DARKEN.style.visibility = "hidden";
+  }, 800);
+
+  setTimeout(() => {
+    RemoveStorageVariable("session", "Mobile Nav Opened");
+    document.body.style.overflowY = "auto";
+    document.body.style.pointerEvents = "auto";
+  }, 1100);
+}
--- assets/functions/dom/togglers/ToggleMobileNav.js
+++ assets/functions/dom/togglers/ToggleMobileNav.js
@@ -6,4 +6,35 @@
 
 import DeclareStorageVariable from "../../data/storage/DeclareStorageVariable";
 
-export default function ToggleMobileNav() {}
+export default function ToggleMobileNav() {
+  const MENU = document.getElementById("mobileNavMenu");
+  const DARKEN = document.getElementById("mobileNavMenuDarken");
+  const MENU_MAIN = document.getElementById("mobileNavMenuMain");
+  const MENU_MAIN_CNT = document.getElementById("mobileNavMenuMainCnt");
+
+  DeclareStorageVariable("session", "Mobile Nav Opened", true);
+
+  document.body.style.overflowY = "hidden";
+  document.body.style.pointerEvents = "none";
+
+  MENU.style.display = "block";
+
+  setTimeout(() => {
+    DARKEN.style.opacity = 1;
+    DARKEN.style.visibility = "visible";
+  }, 400);
+
+  setTimeout(() => {
+    MENU_MAIN.style.transform = "translateX(0)";
+  }, 650);
+
+  setTimeout(() => {
+    MENU_MAIN_CNT.style.opacity = 1;
+    MENU_MAIN_CNT.style.visibility = "visible";
+  }, 1200);
+
+  setTimeout(() => {
+    MENU.style.pointerEvents = "auto";
+    MENU.style.overflowY = "auto";
+  }, 1700);
+}
