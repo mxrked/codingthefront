ed99fe0eb5e177bd37d32cd22e17f0f5f208853e
Parker <pp101socials@gmail.com>
2023-11-22 09:33:14 -0500

    Created file

--- assets/animations/components/FadeDown.js
+++ assets/animations/components/FadeDown.js
@@ -1,20 +1,57 @@
 /**
  *
- *  This is the Fade Down
+ *  This is the Fade Down Transition
  *
  */
 
-import { useRef, useState, useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
+import { motion, useAnimation } from "framer-motion";
 
-import { motion } from "framer-motion";
+// import { FADE_DOWN } from "../objects/FADES";
 
-import { FADE_DOWN } from "../objects/FADES";
-
-export const FadeDown = ({ children }) => {
+export const FadeDown = ({ threshold = 0.5, children }) => {
+  const [HAS_ANIMATED, SET_HAS_ANIMATED] = useState(false);
+  const CONTROLS = useAnimation();
   const REF = useRef(null);
 
+  useEffect(() => {
+    const { offsetTop, offsetHeight } = REF.current;
+
+    const handleScroll = () => {
+      // Making it only display once
+      if (!HAS_ANIMATED && REF.current) {
+        window.requestAnimationFrame(() => {
+          // Getting if the user has scrolled close to the element
+          // const { TOP, BOTTOM } = ELEMENT.getBoundingClientRect();
+          // console.log("Top: " + TOP, "Bottom: " + BOTTOM);
+
+          const IS_VISIBLE =
+            window.scrollY + window.innerHeight >
+            offsetTop + offsetHeight * threshold;
+          console.log("Is Visible: " + IS_VISIBLE);
+
+          // Displaying element
+          if (IS_VISIBLE) {
+            CONTROLS.start({ opacity: 1, y: 0 });
+            SET_HAS_ANIMATED(true);
+          }
+        });
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [CONTROLS, threshold, HAS_ANIMATED]);
+
   return (
-    <motion.div ref={REF} {...FADE_DOWN} className="fm-element fade-down-FM">
+    <motion.div
+      ref={REF}
+      initial={{ opacity: 0, y: -50 }}
+      animate={CONTROLS}
+      transition={{ duration: 0.5 }}
+      className="fm-element"
+    >
       {children}
     </motion.div>
   );
